a2040c2e860075cb00bbfc1667cd4f7b9a3b2bb3
bernardommedeiros <161090624+bernardommedeiros@users.noreply.github.com>
2025-09-08 19:26:32 -0300

    feat: logout

--- src/finances/personal_finance/urls.py
+++ src/finances/personal_finance/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views.auth import register_view, login_view
+from .views.auth import register_view, login_view, logout_view
 from .views.balancete import BalanceteListView, BalanceteCreateView
 from .views.boleto import BoletoListView, BoletoCreateView
+
 
 
 urlpatterns = [
     path('', login_view, name='login'),
     path('register/', register_view, name='register'),
+    path('logout/', logout_view, name='logout'),
     
     path('balancetes/novo/', BalanceteCreateView.as_view(), name='balancete_criar'),
     path('/home/', BalanceteListView.as_view(), name='listar_balancetes'),
